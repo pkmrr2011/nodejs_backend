a5d6422d0ab0d3e94fd758d9a89b23aaaf3b3a8c
pkmrr2011 <pkmrr20@gmail.com>
2024-06-01 23:24:11 +0530

    basic structure

---
+++ app.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import express from 'express'
+// import testRoutes from './routes/test'
+
+const createServer = () => {
+  const app = express()
+
+  app.use(express.json({ limit: '60mb' }))
+  app.use(express.urlencoded({ extended: true }))
+
+  // app.use('/test', testRoutes)
+
+  app.get('/', async (req, res) => {
+    return res.status(200).send({
+      success: true,
+      message: 'The server is running'
+    })
+  })
+
+  app.get('/health', async (req, res) => {
+    return res.status(200).send({
+      success: true,
+      message: 'The server is running'
+    })
+  })
+
+  app.use('*', async (req, res) => {
+    return res.status(404).send({
+      success: false,
+      message: 'URL_NOT_FOUND'
+    })
+  })
+
+  return app
+}
+
+export default createServer
---
+++ index.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+// import { logger } from './shared/logger'
+import createServer from './app'
+
+dotenv.config()
+
+const port = process.env.PORT || 8000
+
+const app = createServer()
+
+try {
+  app.listen(port, () => {
+    console.log(`Connected successfully on port ${port}`)
+  })
+} catch (error) {
+  console.log(`Error occured: ${error.message}`)
+}
